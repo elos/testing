1a1c5426ca4752cecd88e6dc664f0b40a8eb2bbb
Nick Landolfi <nclandolfi@gmail.com>
2015-05-07 15:15:26 -0700

    remove model specific testing

--- expect/errors.go
+++ expect/errors.go
@@ -2,34 +2,7 @@ package expect
 
 import (
 	"testing"
-
-	"github.com/elos/data"
-	"github.com/elos/models"
 )
-
-/*
- is a helper that ensures err
-	is data.ErrAccessDenial and prints a failure message
-	if not
-*/
-func AccessDenial(property string, err error, t *testing.T) {
-	if err != data.ErrAccessDenial {
-		t.Errorf("Expected access denial on %s, got %s", property, err)
-	}
-}
-
-func EmptyLinkError(property string, err error, t *testing.T) {
-	_, ok := err.(*data.EmptyLinkError)
-	if !ok {
-		t.Errorf("Expected empty link on %s, got %s", property, err)
-	}
-}
-
-func EmptyRelationship(property string, err error, t *testing.T) {
-	if err != models.ErrEmptyRelationship {
-		t.Errorf("Expected empty relationship on %s, got %s", property, err)
-	}
-}
 
 func NoError(op string, err error, t *testing.T) {
 	if err != nil {
